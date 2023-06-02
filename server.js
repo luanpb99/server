b9300eb8f50c0050407e398ec4995dd4ecb9313a
DESKTOP-NTJGH68\luan <luanpham.pbl@gmail.com>
2023-06-02 20:01:37 +0700

    init commit

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express')
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server)
+var fs = require('fs');
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+io.on('connection', (socket) => {
+    console.log('A client connected');
+
+    socket.on('chat message', (message) => {
+        console.log('Received message:', message);
+        io.emit('chat message', message);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A client disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    const address = server.address();
+    console.log(`Server is running at http://${address.address}:${address.port}`);
+});
